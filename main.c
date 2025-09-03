75110e0f68eed2768aee96e8d8febe695d6a0af6
Ayman-br <aymanloussal552@gmail.com>
2025-09-03 12:30:38 +0100

    challange5

---
+++ main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+
+int main(){
+float prix = 125;
+int produit = 1 ;
+int tolal;
+printf("how much produit u want");
+scanf("%d", &produit);
+  
+do{
+if
+tolal = tolal + prix - prix* 0.02;
+    
+}while (produit > 1)
+    
+
+
+
+  
+
+
+
+
+   
+
+
+
+
+
+
+return 0;
+}
